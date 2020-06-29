5ae5ed4f517e73af89b9e4a1948c27027baaee32
Oscar Villarroel <oscar.villarroel@clouddistrict.com>
2020-06-29 11:40:23 +0200

    Updates

---
+++ precache-manifest.fcf5cd698b6b9e0ffac82c88f3066764.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54abc534eb9c4334a96e0f1ca4678428",
+    "url": "/gnome-app/index.html"
+  },
+  {
+    "revision": "26252c483ac33dc4f45b",
+    "url": "/gnome-app/static/js/2.4d2a22bc.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/gnome-app/static/js/2.4d2a22bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59c2f4e61ac429f386a0",
+    "url": "/gnome-app/static/js/main.42d50f6f.chunk.js"
+  },
+  {
+    "revision": "feac495b64f5ca839fef",
+    "url": "/gnome-app/static/js/runtime-main.7835b368.js"
+  },
+  {
+    "revision": "f743e406e9df744befe6bfbd2c73e929",
+    "url": "/gnome-app/static/media/39.f743e406.png"
+  },
+  {
+    "revision": "862d721d8e5e1e9f2ecbe8f0b7799bbb",
+    "url": "/gnome-app/static/media/3e9957afe36f31ebb249691f0a9e3098.862d721d.jpg"
+  },
+  {
+    "revision": "9f29f6f5685ea0937b3ffe68954e9354",
+    "url": "/gnome-app/static/media/49095c3ab6870e315be99a7548ae25e7.9f29f6f5.jpg"
+  },
+  {
+    "revision": "af57f8d9eb58bf551b31feb1181fd8d3",
+    "url": "/gnome-app/static/media/9f66e5bc9742c03206b48dfad838849a.af57f8d9.jpg"
+  },
+  {
+    "revision": "a8efcf814e7991aa57a5ec3459e63475",
+    "url": "/gnome-app/static/media/NicePng_batman-png_25335.a8efcf81.png"
+  },
+  {
+    "revision": "b67d3ee3d9dd042c900cc615ec22c369",
+    "url": "/gnome-app/static/media/wonderwoman_2017.b67d3ee3.jpg"
+  }
+]);
